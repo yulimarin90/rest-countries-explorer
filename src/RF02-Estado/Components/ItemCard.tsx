49ee42c56cde5f9bdd4a16a0b2f8ea7e48e9ee1e
Actorsillo99 <scardona120@gmail.com>
2026-09-17 23:15:40 -0500

    chore: Code modification for the rf02-states components.

--- src/RF02-Estado/Components/ItemCard.tsx
+++ src/RF02-Estado/Components/ItemCard.tsx
@@ -8,11 +8,24 @@ interface ItemCardProps {
 function ItemCard({ item }: ItemCardProps) {
   return (
     <article>
-      <h2>{item.name}</h2>
-      <p>ID: {item.id}</p>
-      <p>{item.description}</p>
+      <h2>{item.names.common}</h2>
+      <p>
+        <strong>Región:</strong> {item.region}
+      </p>
+      <p>
+        <strong>Capital: </strong> {' '}
+        {item.capitals.length > 0
+          ? item.capitals.map((capital) => capital.name).join(", ")
+          : "No tiene una capital registrada"}
+      </p>
+        <p>
+            <strong>Idiomas: </strong> {' '}
+            {item.languages.length > 0
+                ? item.languages.map((language) => language.name).join(", ") //usamos .map() porque capitals es un arreglo y recorre capitales
+                : "Sin información disponible"}
+        </p>
     </article>
-  );
+  )
 }
 
 export default ItemCard;
--- src/RF02-Estado/Types/item.ts
+++ src/RF02-Estado/Types/item.ts
@@ -1,5 +1,40 @@
+
+
+export interface Language {
+  name: string
+  native_name: string
+  iso639_1: string
+  iso639_2: string
+  iso639_3: string
+}
+
+export interface Capital {
+  name: string
+  coordinates: {
+    latitude: number
+    longitude: number
+  }
+  attributes: {
+    primary: boolean
+    constitutional: boolean
+    administrative: boolean
+    executive: boolean
+    legislative: boolean
+    judicial: boolean
+  }
+}
+
 export interface Item {
-  id: number;
-  name: string;
-  description: string;
+  names: {
+    common: string
+  }
+  region: string
+  languages: Language[]
+  capitals: Capital[]
+}
+
+export interface CountriesResponse {
+  data: {
+    objects: Item[]
+  }
 }
